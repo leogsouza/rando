276083c6cd5346a085c30c6ed108023ac3e17893
Leonardo Souza <leogsouza@gmail.com>
2017-06-21 00:44:16 -0300

    Setup travis CI

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -4,6 +4,9 @@ const API = 'https://blooming-garden-90086.herokuapp.com/api/passwords';
 
 it('async API call with Jest', async () => {
   const req = await axios.get(API);
-  const result = req.data;
+  const result = req;
+
+  expect.assertions(2);
+  expect(result.status).toBe(200);
   expect(result.data).toHaveLength(5);
 })
