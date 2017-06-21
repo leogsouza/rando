c53bd8f5521aa82c72f795fe39a59cf240dbeaa5
Leonardo Souza <leogsouza@gmail.com>
2017-06-21 15:19:01 -0300

    Add React app

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = { passwords: []}
+
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
+  render() {
+    const { passwords } = this.state;
+    return (
+      <div className="App">
+        {/* Render the passwords if we have them */}
+        {passwords.length ? (
+          <div>
+            <h1>5 Passwords.</h1>
+            <ul className="passwords">
+              {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+            </ul>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Get More
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1>No Passwords :(</h1>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Try Again
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
